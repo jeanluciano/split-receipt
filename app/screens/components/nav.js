e51b229c38458a258c94edaa3ca914756363b094
Rajsekhar Kadiyala <rajsekhar@Rajsekhars-MacBook-Air.local>
2017-08-15 16:23:24 -0500

    abstracted nav out

--- app/screens/components/nav.js
+++ app/screens/components/nav.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { TouchableHighlight } from 'react-native';
 import { Icon } from 'react-native-elements';
 
-const Nav = () => (
-  <TouchableHighlight>
-    <Icon
-      name="navicon"
-      type="evilicon"
-      color="#fff"
-    />
-  </TouchableHighlight>
+const Nav = ({ navigation }) => (
+  <Icon
+    name="navicon"
+    type="evilicon"
+    color="#000"
+    onPress={() => navigation.navigate('DrawerOpen')}
+  />
 );
 
 
