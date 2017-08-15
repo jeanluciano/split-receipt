294be7bf27c1f5bc745af62f3738c53958c73e83
Jean Luciano <jeanlucianoo@gmail.com>
2017-08-15 16:47:28 -0500

    friend add

--- app/redux/friends.js
+++ app/redux/friends.js
@@ -1,10 +1,10 @@
 const CREATE_FRIENDS = 'CREATE_FRIENDS';
-const READ_FRIEDNS = 'READ_FRIENDS';
+const READ_FRIENDS = 'READ_FRIENDS';
 const UPDATE_FRIEND = 'UPDATE_FRIEND';
 const DESTROY_FRIEND = 'DESTROY_FRIENDS';
 
 const createFriends = friend => ({ type: CREATE_FRIENDS, friend });
-const readFriends = friends => ({ type: READ_FRIEDNS, friends });
+const readFriends = () => ({ type: READ_FRIENDS });
 const updateFriends = friend => ({ type: UPDATE_FRIEND, friend });
 const destroyFriend = friend => ({ type: DESTROY_FRIEND, friend });
 
@@ -12,7 +12,7 @@ export default function friendsReducer(state = [], action) {
   switch (action.type) {
     case CREATE_FRIENDS:
       return [...state, ...action.friend];
-    case READ_FRIEDNS:
+    case READ_FRIENDS:
       return [...state];
     case UPDATE_FRIEND:
       return state.map(friend => (friend.recordID === action.friend.recordID ? action.friend : friend));
@@ -23,4 +23,26 @@ export default function friendsReducer(state = [], action) {
   }
 }
 
-export const getFriends 
+export const getFriends = function () {
+  return function thunk(dispatch) {
+    dispatch(readFriends());
+  };
+};
+
+export const addFriend = function (friend) {
+  return function thunk(dispatch) {
+    dispatch(createFriends(friend));
+  };
+};
+
+export const putfriend = function (friend) {
+  return function thunk(dispatch) {
+    dispatch(updateFriends(friend));
+  };
+};
+
+export const deleteFriend = function (friend) {
+  return function thunk(dispatch) {
+    dispatch(destroyFriend(friend));
+  };
+};
--- app/redux/index.js
+++ app/redux/index.js
@@ -2,17 +2,17 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 
-import contacts from './contacts'
-
+import contacts from './contacts';
+import friends from './friends';
 
 const reducer = combineReducers({
   contacts,
+  friends,
 });
 
-const middleware = compose(applyMiddleware(
-  thunkMiddleware,
-  createLogger({ collapsed: true })
-));
+const middleware = compose(
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+);
 
 const store = createStore(reducer, middleware);
 
