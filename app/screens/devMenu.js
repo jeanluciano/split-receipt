93015600ffa1437267a4bed3494c3d5e445c9963
Jason Hu <jhu7235@github.com>
2017-08-18 15:40:24 -0500

    login fix

--- app/screens/devMenu.js
+++ app/screens/devMenu.js
@@ -7,52 +7,55 @@ import { connect } from 'react-redux';
 import { login } from '../redux/auth';
 
 
-const DevMenu = (props) => {
-  return (
+class DevMenu extends Component {
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
     <View className="center" style={{paddingTop: '20%'}}>
-      <Text>DevMenu</Text>
-      <Button
-        title="Log In"
-        color="#841584"
-        onPress={() => navigate('Login')}
-      />
-      <Button
-        title="Send Text"
-        color="#841584"
-        onPress={() => navigate('SendText')}
-      />
-      <Button
-        title="Main"
-        color="#841584"
-        onPress={() => navigate('Main')}
-      />
-      <Button
-        title="Login with Dummy"
-        color="#841584"
-        onPress={() => props.handleLogIn(
-          "jason@one.com",
-          "Jasonone",
-          props.navigation.navigate
-        )}
-      />
-      <Button
-        title="Camera"
-        color="#841584"
-        onPress={() => navigate('Camera')}
-      />
-      <Button
-        title="Stack"
-        color="#841584"
-        onPress={() => navigate('Stack')}
-      />
-      <Button
-        title="Contacts"
-        color="#841584"
-        onPress={() => navigate('Contacts')}
-      />
-    </View>
-  );
-}
+        <Text>DevMenu</Text>
+        <Button
+          title="Log In"
+          color="#841584"
+          onPress={() => navigate('Login')}
+        />
+        <Button
+          title="Send Text"
+          color="#841584"
+          onPress={() => navigate('SendText')}
+        />
+        <Button
+          title="Main"
+          color="#841584"
+          onPress={() => navigate('Main')}
+        />
+        <Button
+          title="Login with Dummy"
+          color="#841584"
+          onPress={() => this.props.handleLogIn(
+            "jason@one.com",
+            "Jasonone",
+            navigate
+          )}
+        />
+        <Button
+          title="Camera"
+          color="#841584"
+          onPress={() => navigate('Camera')}
+        />
+        <Button
+          title="Stack"
+          color="#841584"
+          onPress={() => navigate('Stack')}
+        />
+        <Button
+          title="Contacts"
+          color="#841584"
+          onPress={() => navigate('Contacts')}
+        />
+      </View>
+    );
+  }
+};
 
 
 
