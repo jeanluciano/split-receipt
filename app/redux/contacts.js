bfff3743d72b41132d3deb219d1a67d6fc66fc00
Jean Luciano <jeanlucianoo@gmail.com>
2017-08-18 15:15:00 -0500

    shit works

--- app/redux/contacts.js
+++ app/redux/contacts.js
@@ -7,10 +7,10 @@ const REMOVE_CONTACT = 'REMOVE_CONTACT';
 const readContacts = contacts => ({ type: READ_CONTACTS, contacts });
 const removeContact = contact => ({ type: REMOVE_CONTACT, contact });
 
-export default function contactsReducer(myContacts = [], action) {
+export default function contactsReducer(myContacts = fakeContacts, action) {
   switch (action.type) {
     case READ_CONTACTS:
-      return [action.contacts];
+      return action.contacts;
     case REMOVE_CONTACT:
       return myContacts.filter(contact => contact.recordID !== action.contact.recordID);
     default:
