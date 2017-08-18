fe1cc7c2a79a04ff1c00857b3e14666e24115406
Rajsekhar Kadiyala <rajsekhar@Rajsekhars-MacBook-Air.local>
2017-08-18 14:06:15 -0500

    storing receipt data on redux store

--- app/screens/camera.js
+++ app/screens/camera.js
@@ -1,5 +1,7 @@
 import React, { Component, Dimensions } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { updateReceiptThunkCreator } from '../redux/receipt';
+import { connect } from 'react-redux';
 import Camera from 'react-native-camera';
 import axios from 'axios';
 
@@ -17,6 +19,7 @@ class ReceiptPicture extends Component {
       .then(async (image) => {
         const response = await axios.post('http://' + IP_ADDRESS + ':8000/api/image/receipt', image)
         console.log('we are getting this here', response.data);
+        this.props.dispatchUpdateReceiptThunk(response.data);
       })
       .catch(err => console.error(err));
   }
@@ -57,4 +60,12 @@ const styles = StyleSheet.create({
 })
 
 
-export default ReceiptPicture
+const mapDispatch = dispatch => {
+  return {
+    dispatchUpdateReceiptThunk: (receiptData) => {
+      dispatch(updateReceiptThunkCreator(receiptData));
+    }
+  };
+};
+
+export default connect(null, mapDispatch)(ReceiptPicture);
