c2b2b3728cd490ab142c26ebb8896ffa418f8ec5
rajkadiyala <rskadiy@gmail.com>
2017-08-19 14:22:42 -0500

    populating landing (#85)

--- app/screens/components/nav.js
+++ app/screens/components/nav.js
@@ -1,15 +1,42 @@
 import React from 'react';
-import { TouchableHighlight } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Icon } from 'react-native-elements';
+import LinearGradient from 'react-native-linear-gradient';
+import PropTypes from 'prop-types';
 
-const Nav = ({ navigation }) => (
-  <Icon
-    name="navicon"
-    type="evilicon"
-    color="#000"
-    onPress={() => navigation.navigate('DrawerOpen')}
-  />
-);
+const Nav = (props) => {
 
+  const { backgroundColor, navigation } = props;
+  if (backgroundColor) styles.navicon.backgroundColor = backgroundColor;
+
+  return (
+    <LinearGradient colors={['#8f53f7', '#3d6ddf']} start={[0, 0]} end={[1, 0]} >
+      <Icon
+        name="navicon"
+        type="evilicon"
+        color="#fff"
+        style={ styles.navicon }
+        onPress={() => navigation.navigate('DrawerOpen')}
+      />
+    </LinearGradient>
+  );
+};
+
+Nav.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }),
+  backgroundColor: PropTypes.string,
+};
+
+const styles = StyleSheet.create({
+
+  navicon: {
+    paddingTop: '10%',
+    paddingLeft: '10%',
+    // backgroundColor: '#3d4d65',
+  },
+
+});
 
 export default Nav;
