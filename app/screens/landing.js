f7b8c400f4edd7a13b60a0a242a2cd048c53b752
Rajsekhar Kadiyala <rajsekhar@Rajsekhars-MacBook-Air.local>
2017-08-19 17:59:10 -0500

    modularized landing

--- app/screens/landing.js
+++ app/screens/landing.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Text, View, Button, StyleSheet } from 'react-native';
-import { Icon } from 'react-native-elements';
-import LinearGradient from 'react-native-linear-gradient';
+import { View } from 'react-native';
+// import LinearGradient from 'react-native-linear-gradient';
 import { connect } from 'react-redux';
+
+import CameraLink from './components/Landing/CameraLink';
 
 class Landing extends Component {
 
@@ -12,64 +13,13 @@ class Landing extends Component {
 
   render() {
     return (
-      <View style={styles.outerView} >
-        <View style={styles.innerView}>
-          <Text style={styles.welcomeText}>Hey Raj, do you have a receipt you want to take care of?</Text>
-        </View>
-        <View style={styles.innerView}>
-          <View style={styles.icon}>
-            <Icon
-              name='linked-camera'
-              color='#161338'
-              size={60}
-            />
-          </View>
-        </View>
+      <View>
+      <CameraLink />
       </View>
     );
-
   }
 }
 
-// const mapState = state => {
-//   return {};
-// };
-
-// const mapDispatch = dispatch => {
-//   return {};
-// };
-
-const styles = StyleSheet.create({
-
-  gradientView: {
-  },
-
-  outerView: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-
-  innerView: {
-    flex: 1,
-  },
-
-  welcomeText: {
-    fontSize: 15,
-    alignSelf: 'flex-end',
-    paddingLeft: '30%',
-    paddingBottom: '6%',
-    fontFamily: 'Georgia-Bold',
-    color: '#161338',
-    textAlign: 'right',
-  },
-
-  icon: {
-    flex: 1,
-    paddingLeft: '20%',
-    paddingRight: '30%',
-    alignSelf: 'flex-start',
-  },
-});
 
 export default Landing;
 // export default connect(mapState, mapDispatch)(Landing);
