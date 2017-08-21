fee924e399b8c7cb4cafe83cc8a6978a05f1caa8
Jean Luciano <jeanlucianoo@gmail.com>
2017-08-21 17:50:45 -0500

    Camerastyling (#102)
    * camera button styling
    
    * menu icon add
    
    * styling done

--- app/screens/Camera.js
+++ app/screens/Camera.js
@@ -1,24 +1,29 @@
 import React, { Component, Dimensions } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { Icon } from 'react-native-elements';
 import { updateReceiptThunkCreator } from '../redux/receipt';
 import { connect } from 'react-redux';
+import LinearGradient from 'react-native-linear-gradient';
 import Camera from 'react-native-camera';
 import axios from 'axios';
-
-const IP_ADDRESS = '172.28.116.54'
+import Menu from './components/Nav';
+const IP_ADDRESS = '172.28.116.54';
 
 class ReceiptPicture extends Component {
-
   constructor(props) {
     super(props);
     this.takePicture = this.takePicture.bind(this);
   }
 
   takePicture() {
-    this.camera.capture({ rotation: 270 })
-      .then(async (image) => {
+    this.camera
+      .capture({ rotation: 270 })
+      .then(async image => {
         console.log('something is happening');
-        const response = await axios.post('http://' + IP_ADDRESS + ':8000/api/image/receipt', image)
+        const response = await axios.post(
+          'http://' + IP_ADDRESS + ':8000/api/image/receipt',
+          image,
+        );
         console.log('we are getting this here', response.data);
         this.props.dispatchUpdateReceiptThunk(response.data);
       })
@@ -28,15 +33,25 @@ class ReceiptPicture extends Component {
   render() {
     return (
       <Camera
-        ref={cam => {this.camera = cam;}}
+        ref={cam => {
+          this.camera = cam;
+        }}
         style={styles.preview}
         aspect={Camera.constants.Aspect.fill}
         captureTarget={Camera.constants.CaptureTarget.memory}
         orientation={Camera.constants.Orientation.portrait}
       >
-        <Text style={styles.capture} onPress={this.takePicture}>
-          [CAPTURE]
-        </Text>
+        <Icon
+          containerStyle={styles.menu}
+          name="navicon"
+          type="evilicon"
+          color="#29D168"
+          size={40}
+          onPress={()=> this.props.navigation.navigate('DrawerOpen')}
+        />
+        <LinearGradient style={styles.capture} colors={['#29D168', '#0081D5']}>
+          <Icon name="camera" size={70} color="white" />
+        </LinearGradient>
       </Camera>
     );
   }
@@ -47,25 +62,31 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'center',
-    height: '100%',
-    width: '100%',
   },
   capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    color: '#000',
-    padding: 10,
+    width: 70,
+    height: 70,
+    borderRadius: 35,
     margin: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    borderWidth: 9,
+    borderColor: 'black',
+  },
+  menu : {
+    bottom: '66%',
+    alignSelf: 'flex-start',
+    padding: '5%'
   }
-})
-
+});
 
 const mapDispatch = dispatch => {
   return {
-    dispatchUpdateReceiptThunk: (receiptData) => {
+    dispatchUpdateReceiptThunk: receiptData => {
       dispatch(updateReceiptThunkCreator(receiptData));
-    }
+    },
   };
 };
 
