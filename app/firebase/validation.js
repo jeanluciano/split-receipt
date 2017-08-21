2c238d842e1a2cda1d42602008a09ccc41b9f396
Jason Hu <jhu7235@gmail.com>
2017-08-21 11:03:26 -0500

    avoid duplicating variable setting (#96)

--- app/firebase/validation.js
+++ app/firebase/validation.js
@@ -1,13 +1,20 @@
 export const validateShape = function(obj, type) {
+  let {
+    total,
+    status,
+    date,
+    receiptId,
+    origin, destination,
+    items,
+    transactions,
+    payer,
+  } = obj;
   switch(type) {
     case 'TRANSACTION':
-      const {total, status, date, receiptId, origin, destination, items } = obj
       return (total && status && date && receiptId && origin && destination);
     case 'USER':
-      const {total, status, date, receiptId, origin, destination, items } = obj
       return (total && status && date && receiptId && origin && destination);
     case 'RECEIPT':
-      const { date, payer, total, transactions } = obj
       return (date && payer && total && transactions);
     default:
       return false;
