c02ced912d7de5c73e2991c3bd7d6eb826bec5ee
Rajsekhar Kadiyala <rskadiy@gmail.com>
2017-08-23 14:34:53 -0500

    changed relevant colors

--- app/screens/components/Initial/Explanation2.js
+++ app/screens/components/Initial/Explanation2.js
@@ -67,6 +67,7 @@ const styles = StyleSheet.create({
     paddingRight: '10%',
     paddingTop: '3%',
     textAlign: 'center',
+    color: '#a9b9ca',
   }
 
 });
--- app/screens/components/Initial/Title.js
+++ app/screens/components/Initial/Title.js
@@ -44,6 +44,7 @@ const styles = StyleSheet.create({
     paddingRight: '10%',
     paddingTop: '3%',
     textAlign: 'center',
+    color: '#a9b9ca',
   }
 
 });
