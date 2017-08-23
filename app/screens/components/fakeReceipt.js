1b1571587e7454fc3ac6664ef53f2b5d46cc9872
Jean Luciano <jeanlucianoo@gmail.com>
2017-08-23 14:47:17 -0500

    send text stylked (#123)

--- app/screens/components/fakeReceipt.js
+++ app/screens/components/fakeReceipt.js
@@ -18,30 +18,10 @@ const fakeReceipt = [
     id: 1232
   },
   {
-    price: 25.14,
-    item: 'Total',
-  },
-  {
     price: 10.95,
     item: '  Szech Eggplant',
     duplicity: 1,
     id: 'asd1234lkjs',
-  },
-  {
-    price: 0.65,
-    item: ' White Rice',
-    duplicity: 1,
-    id: 'asdflkj12',
-  },
-  {
-    price: 0,
-    item: '  *Spring Rolls',
-    duplicity: 1,
-    id: 'alsk1231',
-  },
-  {
-    price: 25.14,
-    item: 'total',
   },
 ];
 
