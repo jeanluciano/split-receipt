8128b767c2b33572868487fa0527ff2b01171199
Rajsekhar Kadiyala <rskadiy@gmail.com>
2017-08-27 04:18:01 -0500

    started transactions

--- app/screens/Transactions.js
+++ app/screens/Transactions.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { colors } from '../values/stylesheet';
@@ -7,27 +7,42 @@ import { colors } from '../values/stylesheet';
 class Transactions extends Component {
   constructor() {
     super();
+    this.state = {
+      active: 'all',
+    };
   }
+
+  // toggleActive(){
+  //   const active = this.state.active;
+  //   this.refs.active.style = styles.
+  //   this.state.active.
+  // }
+
 
   render() {
     return (
-      <View style={styles.outerView}>
-      <View style={styles.slideUpView}>
-        <Icon
-          name="chevron-down"
-          type="font-awesome"
-          color={colors.splitGray}
-          size={12}
-        />
-        <Text> Slide down for camera view </Text>
-        <Icon
-          name="chevron-down"
-          type="font-awesome"
-          color={colors.splitGray}
-          size={12}
-        />
+      <ScrollView contentContainerStyle={styles.outerView}>
+        <View style={styles.slideUpView}>
+          <Icon
+            name="chevron-down"
+            type="font-awesome"
+            color={colors.splitGray}
+            size={12}
+          />
+          <Text> Slide down for camera view </Text>
+          <Icon
+            name="chevron-down"
+            type="font-awesome"
+            color={colors.splitGray}
+            size={12}
+          />
         </View>
-      </View>
+        <View style={styles.tabsView}>
+          <TouchableOpacity ref="all" onPress={(props) => console.log('hello', props)}><Text>Icon 1</Text></TouchableOpacity>
+          <TouchableOpacity ref="out" onPress={(props) => console.log('hello', props)}><Text>Icon 2</Text></TouchableOpacity>
+          <TouchableOpacity ref="in" onPress={(props) => console.log('gello', props)}><Text>Icon 3</Text></TouchableOpacity>
+        </View>
+      </ScrollView>
 
 
     );
@@ -41,8 +56,6 @@ const styles = StyleSheet.create({
 
   outerView: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'flex-start',
   },
 
   slideUpView: {
@@ -50,5 +63,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
+  tabsView: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    borderWidth: 1,
+    borderRadius: 10,
+    marginLeft: '5%',
+    marginRight: '5%',
+  },
+
+  activeTab: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'blue',
+  },
 
 });
