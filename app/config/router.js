052ec42ab0b7d05717940d340d20c7586059de67
Rajsekhar Kadiyala <rskadiy@gmail.com>
2017-08-27 12:44:29 -0500

    transaction thingies

--- app/config/router.js
+++ app/config/router.js
@@ -61,9 +61,6 @@ export default DrawerNavigator({
   Splash: {
     screen: Splash,
   },
-  Transactions: {
-    screen: Transactions,
-  },
   Landing: {
     screen: menuify(Landing, ['#fff', '#cad3df', '#fff']),
   },
