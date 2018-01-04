197cd40ca566665bfa91d2ed02c71ac576db3be8
Jason Hu <jhu7235@gmail.com>
2018-01-03 20:45:46 -0600

    fix: sign-in

--- app/redux/auth.js
+++ app/redux/auth.js
@@ -46,11 +46,11 @@ export const logout = () =>
       .then(() => dispatch(removeUser()))
 
 export const signup = (email, password, givenName, familyName) =>
-  dispatch => {
-    console.log('email, password, givenName, familyName', email, password, givenName, familyName);
-    // firebaseSignUp(email, password, givenName, familyName)
-    //   .then(user => dispatch(updateUser(user)))
-    //   .catch(console.error)
+  (dispatch) => {
+    // console.log('email, password, givenName, familyName', email, password, givenName, familyName);
+    firebaseSignUp(email, password, givenName, familyName)
+      .then(user => dispatch(updateUser(user)))
+      .catch(console.error)
   }
 
 export const userUpdate = (userId, property) =>
